9ed87bf411d8a19b5a87766d07f0e663d462befb
Bulbizar <adrienesteves@icloud.com>
2023-10-04 10:29:33 +0200

    Home Page base

--- the-idle-game/src/App.vue
+++ the-idle-game/src/App.vue
@@ -1,70 +1,53 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="gold">
+      <h3><a>Or possédé : </a></h3>
+    </div>
+    <div>
+      <h3><a>Elixir possédé :</a></h3>
+    </div>
+    <div>
+      <h3><a>Inventaire</a></h3>
+    </div>
+    <div>
+      <h3><a>Boutique</a></h3>
+    </div>
+    <div>
+      <h3><a>Défis</a></h3>
+    </div>
+    <div>
+      <h3><a>Historique</a></h3>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
   max-height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 10px;
 }
 
-.logo {
+header div {
   display: block;
-  margin: 0 auto 2rem;
+  margin-bottom: 10px;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
+header div h3 {
+  cursor: pointer;
 }
 
 @media (min-width: 1024px) {
   header {
-    display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
